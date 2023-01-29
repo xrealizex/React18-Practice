e390347ada854e8f25c30fbedcebb94849046597
hiroto-hara <xre0605@gmail.com>
2023-01-29 16:03:49 +0900

    understand strict mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("Appがレンダリングされました")
+  useEffect(
+    () => {console.log("useEffect")},[]
+  )
   return (
     <div className="App">
       <header className="App-header">
