2b8f7fedfa6f4b915fbfb1e1f848074dd06596e2
hiroto-hara <xre0605@gmail.com>
2023-01-29 18:41:25 +0900

    understand Transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import { AutoBatchEventHandler } from './components/AutoBatchEventHandler';
 import { AutoBatchOther } from './components/AutoBatchOther';
+import { Transition } from './components/Transition';
 
 function App() {
   return (
     <div className="App">
       <AutoBatchEventHandler/>
       <AutoBatchOther/>
+      <hr />
+      <Transition/>
     </div>
   );
 }
